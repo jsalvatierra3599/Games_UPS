6d0ea3db8f82cf66d1e645e66a0863f756f9cc27
Jose Salvatierra Parra <94132050+jsalvatierra3599@users.noreply.github.com>
2022-09-19 18:39:29 -0500

    SPACE INVADERS
    JUEGO U3-TALLER 02

---
+++ code/spaceinvaders-JoseSalvatierra/ejemplo_02/invasor.py
@@ -0,0 +1,39 @@
+# //NOMBRE: JOSE SALVATIERRA
+# //[U3_TAREA_02]: Space Invaders
+# //PROGRAMACION HIPERMEDIAL
+
+import pygame
+
+class Invader( pygame.sprite.Sprite ):
+    def __init__(self, game, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('./images/spaceInvaders.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.move_direction = 1
+        self.move_counter = 0
+        self.game = game
+    
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+
+        if self.move_counter > 75:
+            self.move_direction *= -1
+            self.move_counter *= -1
+        
+
+class Invader_Bullet(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('./images/invader_bullet.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.game = game
+
+    def update(self):
+        self.rect.y += 2
+
+        if pygame.sprite.spritecollide(self, self.game.player_group, False):
+            self.kill()
+            self.game.player.health_remaining -= 10
